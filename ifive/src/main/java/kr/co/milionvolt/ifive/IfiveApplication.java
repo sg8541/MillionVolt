025bd63b9b940c1062c6c964dbdc192c22b0dcdf
ksks1234 <logging2021@gmail.com>
2024-12-01 16:43:39 +0900

    init: 새로운 프로젝트 생성

---
+++ ifive/src/main/java/kr/co/milionvolt/ifive/IfiveApplication.java
@@ -0,0 +1,13 @@
+package kr.co.milionvolt.ifive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfiveApplication.class, args);
+	}
+
+}
---
+++ ifive/src/test/java/kr/co/milionvolt/ifive/IfiveApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.milionvolt.ifive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
