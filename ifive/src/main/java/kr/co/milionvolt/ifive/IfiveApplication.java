4faa7a704a74d179dc708a7cfd279bb5c3aaff4c
ksks1234 <logging2021@gmail.com>
2024-12-02 01:35:08 +0900

    fix: security 작업 중

--- ifive/src/main/java/kr/co/milionvolt/ifive/IfiveApplication.java
+++ ifive/src/main/java/kr/co/milionvolt/ifive/IfiveApplication.java
@@ -1,9 +1,11 @@
 package kr.co.milionvolt.ifive;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(value={"mybatis.dao"})
 public class IfiveApplication {
 
 	public static void main(String[] args) {
