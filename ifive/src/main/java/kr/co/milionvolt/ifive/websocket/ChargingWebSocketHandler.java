e387a148fb13e3bef459891b8239c79b3bfcc80e
KangJaeHeon <gjsdms1244@gmail.com>
2024-12-05 10:00:04 +0900

    feat : 웹소켓 config 설정 및 구현

--- ifive/src/main/java/kr/co/milionvolt/ifive/websocket/ChargingWebSocketHandler.java
+++ ifive/src/main/java/kr/co/milionvolt/ifive/websocket/ChargingWebSocketHandler.java
@@ -1,7 +1,8 @@
-package kr.co.milionvolt.ifive.config;
+package kr.co.milionvolt.ifive.websocket;
 
 import kr.co.milionvolt.ifive.service.charging.ChargingStatusSerivce;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
@@ -26,9 +27,23 @@ public class ChargingWebSocketHandler extends TextWebSocketHandler {
         String payload = message.getPayload();
         System.out.println("메세지 수신 : "+payload);
 
+        if ("start".equals(payload)) {
+            startCharging(session);
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception{
+        System.out.println("웹소켓 연결 종료" + session.getId());
+        sessions.remove(session.getId());
+
     }
 
     private void sendChargingStatus(WebSocketSession session) {
+
+
+    }
+    private void startCharging(WebSocketSession session) {
 
 
     }
--- ifive/src/main/java/kr/co/milionvolt/ifive/websocket/WebSocketConfig.java
+++ ifive/src/main/java/kr/co/milionvolt/ifive/websocket/WebSocketConfig.java
@@ -1,7 +1,6 @@
-package kr.co.milionvolt.ifive.config;
+package kr.co.milionvolt.ifive.websocket;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.socket.EnableWebSocketSecurity;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
