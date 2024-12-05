c09a0c9473ea89f2a884dfa8b0c8ed4df1f4a4d0
ksks1234 <logging2021@gmail.com>
2024-12-05 11:50:47 +0900

    Init: frontend 폴더 구성 + 마이페이지 레이아웃 완성

---
+++ frontend/src/layouts/components/DefaultLayoutWithVerticalNav.vue
@@ -0,0 +1,114 @@
+<script setup>
+import { useTheme } from 'vuetify'
+import VerticalNavSectionTitle from '@/@layouts/components/VerticalNavSectionTitle.vue'
+import VerticalNavLayout from '@layouts/components/VerticalNavLayout.vue'
+import VerticalNavLink from '@layouts/components/VerticalNavLink.vue'
+
+// Components
+import NavbarThemeSwitcher from '@/layouts/components/NavbarThemeSwitcher.vue'
+import UserProfile from '@/layouts/components/UserProfile.vue'
+
+const vuetifyTheme = useTheme()
+
+</script>
+
+<template>
+  <VerticalNavLayout>
+    <!-- 👉 navbar -->
+    <template #navbar="{ toggleVerticalOverlayNavActive }">
+      <div class="d-flex h-100 align-center">
+        <!-- 👉 Vertical nav toggle in overlay mode -->
+        <IconBtn
+          class="ms-n3 d-lg-none"
+          @click="toggleVerticalOverlayNavActive(true)"
+        >
+          <VIcon icon="bx-menu" />
+        </IconBtn>
+
+        <VSpacer />
+
+        <NavbarThemeSwitcher class="me-2" />
+
+        <UserProfile />
+      </div>
+    </template>
+
+    <template #vertical-nav-content>
+      <VerticalNavLink
+        :item="{
+          title: 'Dashboard',
+          icon: 'bx-home',
+          to: '/dashboard',
+        }"
+      />
+      <VerticalNavLink
+        :item="{
+          title: '내 정보',
+          icon: 'mdi-account-cog-outline',
+          to: '/account-settings',
+        }"
+      />
+      <VerticalNavLink
+        :item="{
+          title: '나의 예약 정보',
+          icon: 'bx-time',
+          to: '/my-reservation',
+        }"
+      />
+      <VerticalNavLink
+        :item="{
+          title: '나의 결제 정보',
+          icon: 'bx-wallet-alt',
+          to: '/my-payment',
+        }"
+      />
+
+      <!-- 👉 Pages -->
+      <VerticalNavLink
+        :item="{
+          title: 'Error',
+          icon: 'bx-info-circle',
+          to: '/no-existence',
+        }"
+      />
+
+      <!-- 👉 User Interface -->
+      <VerticalNavSectionTitle
+        :item="{
+          heading: 'User Interface',
+        }"
+      />
+
+    </template>
+
+    <template #after-vertical-nav-items>
+      <!-- 👉 illustration -->
+      <a
+        href="https://themeselection.com/item/sneat-vuetify-vuejs-admin-template"
+        target="_blank"
+        rel="noopener noreferrer"
+        style="margin-left: 7px;"
+      >
+      </a>
+    </template>
+
+    <!-- 👉 Pages -->
+    <slot />
+
+    <!-- 👉 Footer -->
+    <!-- <template #footer>
+      <Footer />
+    </template> -->
+  </VerticalNavLayout>
+</template>
+
+<style lang="scss" scoped>
+.meta-key {
+  border: thin solid rgba(var(--v-border-color), var(--v-border-opacity));
+  border-radius: 6px;
+  block-size: 1.5625rem;
+  line-height: 1.3125rem;
+  padding-block: 0.125rem;
+  padding-inline: 0.25rem;
+}
+</style>
