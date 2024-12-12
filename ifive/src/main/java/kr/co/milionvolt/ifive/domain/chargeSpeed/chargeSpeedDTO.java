424a8d82a64e16c8b34063a4e49c425090e4b62c
kyunga1126 <audwk6028@naver.com>
2024-12-12 10:14:05 +0900

    ãmainPage 테스트

---
+++ ifive/src/main/java/kr/co/milionvolt/ifive/domain/chargeSpeed/chargeSpeedDTO.java
@@ -0,0 +1,13 @@
+package kr.co.milionvolt.ifive.domain.chargeSpeed;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class chargeSpeedDTO {
+    private Integer chargerSpeedId;
+    private String chargerSpeed;
+}
--- ifive/src/main/java/kr/co/milionvolt/ifive/domain/charger/ChargerVO.java
+++ ifive/src/main/java/kr/co/milionvolt/ifive/domain/charger/ChargerVO.java
@@ -4,24 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.sql.Timestamp;
+
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class ChargerVO {
     private Integer chargerId;
     private Integer stationId;
-    private ChagerType chagerType;
-    private ChargerStatus chargerStatus;
-
-    public enum ChagerType {
-        SEVEN_KW,  // 7kW
-        FIFTY_KW,  // 50kW
-        ONE_HUNDRED_KW, // 100kW
-        TWO_HUNDRED_KW,  // 200kW
-        THREE_HUNDRED_PLUS_KW // 300kW+
-    }
-
-    public enum ChargerStatus {
-        available, in_use, maintenance
-    }
+    private Integer chargerStatusId;
+    private Integer chargerSpeedId;
 }
