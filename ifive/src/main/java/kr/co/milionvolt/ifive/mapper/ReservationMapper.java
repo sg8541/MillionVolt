3bdf2c3fe3e49e756e8694182ca0ef27fefb5438
KimDoHun <thurr1394@gamil.com>
2024-12-17 17:00:52 +0900

    fix : reservation(신규 예약, 기존 예약의 시작 시간과 종료 시간 15분 전후 예약 충돌 확인)로직 구현해서 추가

--- ifive/src/main/java/kr/co/milionvolt/ifive/mapper/ReservationMapper.java
+++ ifive/src/main/java/kr/co/milionvolt/ifive/mapper/ReservationMapper.java
@@ -4,6 +4,7 @@ import kr.co.milionvolt.ifive.domain.reservation.ReservationDTO;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 //@Mapper
 //public interface ReservationMapper {
@@ -44,6 +45,34 @@ public interface ReservationMapper {
             " OR  (DATE(start_time) = DATE(#{startTime}) " +
             " AND TIME(start_time) <= TIME(#{startTime}) " +
             " AND TIME(end_time) >= TIME(#{endTime})) " +
-            " AND charger_id = #{chargerId}")
+            " AND charger_id = #{chargerId}" +
+            " AND status = 'confirmed'")
     int checkConflictReservation(ReservationDTO reservationDTO);
+
+//    @Select(" SELECT count(*) " +
+//            " FROM reservation " +
+//            " WHERE (DATE(start_time) = DATE(#{startTime})" +
+//            " AND (TIME(start_time)-15) BETWEEN TIME(#{startTime}) AND TIME(#{endTime}))" +
+//            " OR (DATE(end_time) = DATE(#{startTime})" +
+//            " AND TIME((end_time)+15) BETWEEN TIME(#{startTime}) AND TIME(#{endTime})) " +
+//            " OR (DATE(start_time) = DATE(#{startTime})" +
+//            " AND TIME(start_time) <= TIME(#{startTime})" +
+//            " AND TIME(end_time) >= TIME(#{endTime}))" +
+//            " AND chareg_id = #{chargerId}")
+//    int checkConflictReservationPlusFifteen(ReservationDTO reservationDTO);
+
+    @Select(" SELECT count(*) " +
+            " FROM reservation " +
+            " WHERE DATE(start_time) = DATE(#{startTime}) " +
+            " AND charger_id = #{chargerId} " +
+            " AND status = 'confirmed' " +
+            " AND ( " +
+            "     (TIME(#{startTime}) BETWEEN TIME(start_time) AND ADDTIME(TIME(end_time), '00:15:00')) " +
+            "     OR (TIME(#{endTime}) BETWEEN SUBTIME(TIME(start_time), '00:15:00') AND TIME(end_time)) " +
+            "     OR (TIME(start_time) <= TIME(#{startTime}) AND TIME(end_time) >= TIME(#{endTime})) " +
+            "     OR (TIME(#{endTime}) <= SUBTIME(TIME(start_time), '00:15:00')) " +
+            " )")
+    int checkConflictReservationPlusFifteen(ReservationDTO reservationDTO);
+
+
 }
